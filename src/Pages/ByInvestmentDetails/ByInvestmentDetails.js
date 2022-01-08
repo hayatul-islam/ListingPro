ad1f963282f7e44666131e57a08892a4b9a0791b
hayatulemon <hayatulemon340@gmail.com>
2022-01-08 20:37:32 +0600

    pagenate add

--- src/Pages/ByInvestmentDetails/ByInvestmentDetails.js
+++ src/Pages/ByInvestmentDetails/ByInvestmentDetails.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
+import ReactPaginate from 'react-paginate';
 import { useParams } from 'react-router-dom';
 import Listing from '../Home/Listing/Listing';
 import SearchListing from '../Listings/SearchListing/SearchListing';
@@ -14,10 +15,18 @@ const ByInvestmentDetails = () => {
             .then(res => res.json())
             .then(data => {
                 const filterData = data?.filter(cat => parseFloat(cat?.totalCash) <= parseFloat(level));
-                console.log((filterData));
                 setInvestmentLevel(filterData);
             })
     }, [investmentLevel]);
+
+    const [pageNumber, setPageNumber] = useState(0);
+
+    const perPage = 16;
+    const pagesVisited = pageNumber * perPage;
+    const pageCount = Math.ceil(investmentLevel.length / perPage);
+    const changePage = ({ selected }) => {
+        setPageNumber(selected)
+    }
 
     return (
         <div className='px-3 py-5'>
@@ -27,13 +36,28 @@ const ByInvestmentDetails = () => {
                 </div>
                 <Row>
                     {
-                        investmentLevel?.map(listing => <Col
+                        investmentLevel.slice(pagesVisited, pagesVisited + perPage)?.map(listing => <Col
                             key={listing?._id}
                             xs={12} md={3}>
                             <Listing listing={listing} />
                         </Col>)
                     }
                 </Row>
+
+                <div className="pt-5 d-flex justify-content-center">
+                    <ReactPaginate
+                        previousLabel={"Previous"}
+                        nextLabel={"Next"}
+                        pageCount={pageCount}
+                        onPageChange={changePage}
+                        containerClassName={"paginationBttns"}
+                        previousLinkClassName={"previousBttn"}
+                        nextLinkClassName={"nextBttn"}
+                        disabledClassName={"paginationDisabled"}
+                        activeClassName={"paginationActive"}
+                    />
+                </div>
+
             </Container>
         </div>
     );
--- src/Pages/Industry/IndustryDetails/IndustryDetails.js
+++ src/Pages/Industry/IndustryDetails/IndustryDetails.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
+import ReactPaginate from 'react-paginate';
 import { useParams } from 'react-router-dom';
 import useListing from '../../../Hooks/useListing';
 import Listing from '../../Home/Listing/Listing';
@@ -12,6 +13,14 @@ const IndustryDetails = () => {
 
     const filterListing = listing?.filter(list => list?.category === industryName);
 
+    const [pageNumber, setPageNumber] = useState(0);
+
+    const perPage = 16;
+    const pagesVisited = pageNumber * perPage;
+    const pageCount = Math.ceil(filterListing.length / perPage);
+    const changePage = ({ selected }) => {
+        setPageNumber(selected)
+    }
 
 
     return (
@@ -20,13 +29,28 @@ const IndustryDetails = () => {
                 <SearchListing />
                 <Row>
                     {
-                        filterListing?.map(listing => <Col
+                        filterListing.slice(pagesVisited, pagesVisited + perPage)?.map(listing => <Col
                             key={listing?._id}
                             xs={12} md={3}>
                             <Listing listing={listing} />
                         </Col>)
                     }
                 </Row>
+
+                <div className="pt-5 d-flex justify-content-center">
+                    <ReactPaginate
+                        previousLabel={"Previous"}
+                        nextLabel={"Next"}
+                        pageCount={pageCount}
+                        onPageChange={changePage}
+                        containerClassName={"paginationBttns"}
+                        previousLinkClassName={"previousBttn"}
+                        nextLinkClassName={"nextBttn"}
+                        disabledClassName={"paginationDisabled"}
+                        activeClassName={"paginationActive"}
+                    />
+                </div>
+
             </Container>
         </div>
     );
