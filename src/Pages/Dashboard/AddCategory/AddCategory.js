30f140731e8841cd8ea8ce874f2b11b7e493d13a
hayatulemon <hayatulemon340@gmail.com>
2022-01-10 19:41:02 +0600

    add category image file upload

--- src/Pages/Dashboard/AddCategory/AddCategory.js
+++ src/Pages/Dashboard/AddCategory/AddCategory.js
@@ -1,44 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Col, Row } from 'react-bootstrap';
-import axios from 'axios';
 import { useForm } from 'react-hook-form';
 
 const AddCategory = () => {
 
     const { register, handleSubmit, reset } = useForm();
+    const [saveImage, setSaveImage] = useState();
 
-    const onSubmit = data => {
-        console.log(data);
-        axios.post('https://aqueous-garden-52898.herokuapp.com/addCategory', data)
+    const onSubmit = (data => {
+        data.image = saveImage;
+        fetch('https://aqueous-garden-52898.herokuapp.com/addCategory', {
+            method: 'POST',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
             .then((result) => {
-                if (result.data.insertedId) {
-                    reset();
+                if (result.insertedId) {
+                    reset()
                 }
             })
+
+    });
+
+
+    const imageUploader = async (e) => {
+        const base64 = await convertBase64(e.target.files[0]);
+        setSaveImage(base64);
     };
 
-    // const onSubmit = async (data) => {
-    //     if (!editService && !data.image[0]) {
-    //       return toast.error("Please upload an image!");
-    //     }
-    //     const loading = toast.loading("Uploading...Please wait!");
-    //     let imageURL = editService ? editService.image : "";
-
-    //     if (!editService || (editService && data.image[0])) {
-    //       const imageData = new FormData();
-    //       imageData.set("key", "08d5da1c81cc5c52012f0b930505d031");
-    //       imageData.append("image", data.image[0]);
-    //       try {
-    //         const res = await axios.post(
-    //           "https://api.imgbb.com/1/upload",
-    //           imageData
-    //         );
-    //         imageURL = res.data.data.display_url;
-    //       } catch (error) {
-    //         toast.dismiss(loading);
-    //         return toast.error("Failed to upload the image!");
-    //       }
-    //     }
+    const convertBase64 = (file) => {
+        return new Promise((resolve, reject) => {
+            const fileReader = new FileReader();
+            fileReader.readAsDataURL(file);
+            fileReader.onload = () => {
+                resolve(fileReader.result);
+            };
+            fileReader.onerror = (error) => {
+                reject(error);
+            };
+        });
+    };
 
 
     return (
@@ -57,8 +59,8 @@ const AddCategory = () => {
                                 </Col>
                                 <Col xs={12} md={6}>
                                     <div className='pb-2'>
-                                        <label className='mb-2' htmlFor="">Brand Image</label> <br />
-                                        <input type="text" className='w-100 mb-2 py-1' {...register("image", { required: true })} placeholder='Enter brand image url' />
+                                        <label className='mb-2' htmlFor="">Category Image</label> <br />
+                                        <input onChange={imageUploader} type="file" className='w-100 mb-2 py-1' placeholder='Enter Category image' />
                                     </div>
                                 </Col>
                             </Row>
--- src/Pages/Dashboard/Test/Test.js
+++ src/Pages/Dashboard/Test/Test.js
@@ -1,52 +1,28 @@
-import React, {useState} from 'react';
-import { useForm } from 'react-hook-form';
-import axios from 'axios';
 
-const Test = () => {
-
-    const { register, handleSubmit, reset } = useForm();
-    const [saveImage, setSaveImage] = useState('');
-
-    const onSubmit = data => {
-        console.log(data);
-        axios.post('http://localhost:4040/fileUpload', data)
-            .then((result) => {
-                if (result.data.insertedId) {
-                    reset();
-                }
-            })
-    };
-
-console.log(saveImage);
-
-     const handleImageUpload = async (e) => {
+import { useState } from "react";
+export default function Test() {
+    const [saveImage, setSaveImage] = useState();
+    const imageUploader = async (e) => {
         const base64 = await convertBase64(e.target.files[0]);
-        setSaveImage(base64)
-    }
-    
+        setSaveImage(base64);
+    };
+    console.log(saveImage)
+
     const convertBase64 = (file) => {
-    return new Promise((resolve, reject) => {
-        const fileReader = new FileReader();
-        fileReader.readAsDataURL(file);
-        fileReader.onload = () => {
-            resolve(fileReader.result)
-            console.log(fileReader.result);
-        }
-        fileReader.onerror = ((error) => {
-            reject(error);
-            console.log(error);
-        })
-    })
-}
-
-
+        return new Promise((resolve, reject) => {
+            const fileReader = new FileReader();
+            fileReader.readAsDataURL(file);
+            fileReader.onload = () => {
+                resolve(fileReader.result);
+            };
+            fileReader.onerror = (error) => {
+                reject(error);
+            };
+        });
+    };
     return (
-        <div>
-                <input onClick={handleImageUpload} type="file" name="image" />
-                <input type="submit" value="Send" />
-            
+        <div className="App">
+            <input type="file" onChange={imageUploader} />
         </div>
     );
-};
-
-export default Test;
+}
